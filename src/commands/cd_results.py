0965ab9d9d28b02b14c0879c6354ade9ec5064f9
Håkon Austlid Taskén <hakon.tasken@gmail.com>
2018-10-06 18:28:00 +0200

    Add '-n' as optilan arguments and '-n 0' as default.
    'cd_results' without any parameters now goes to results of last
    simulation.

--- src/commands/cd_results.py
+++ src/commands/cd_results.py
@@ -11,8 +11,9 @@ import os
 
 def command_line_arguments_parser():
     # yapf: disable
-    parser = argparse.ArgumentParser(description="Return full path to 'result_dir' of simulation with provided id, '-i ID'. Used by a bash function, 'cd_results', to change directory to this 'result_dir'.")
-    parser.add_argument('--id', '-i', type=int, required=True, help="<Required> 'ID' of the 'result_dir' in the 'sim.db' database.")
+    parser = argparse.ArgumentParser(description="Return full path to 'result_dir' of simulation specified, of last entry if unspecified. Used by a bash function, 'cd_results', to change directory to this 'result_dir'.")
+    parser.add_argument('--id', '-i', type=int, help="'ID' of the 'result_dir' in the 'sim.db' database.")
+    parser.add_argument('-n', type=int, help="n'th last entry in the 'sim.db' database. (zero indexed)") 
     # yapf: enable
 
     return parser
@@ -24,14 +25,27 @@ def cd_results(argv=None):
     db = helpers.connect_sim_db()
     db_cursor = db.cursor()
 
-    db_cursor.execute("SELECT result_dir FROM runs WHERE id={0}".format(
-            args.id))
-    result_dir = db_cursor.fetchone()[0]
+    if args.id != None:
+        db_cursor.execute("SELECT result_dir FROM runs WHERE id={0}".format(
+                args.id))
+        result_dir = db_cursor.fetchone()[0]
+    else:
+        db_cursor.execute("SELECT result_dir FROM runs")
+        if args.n == None:
+            args.n = 0
+        result_dir = db_cursor.fetchall()
+        if len(result_dir) > args.n:
+            result_dir = result_dir[len(result_dir) - args.n - 1][0]
 
     db.commit()
     db_cursor.close()
     db.close()
 
+    if result_dir == None:
+        print("Specified simulation does not have a 'result_dir' path.")
+        exit()
+
+    result_dir.replace(" ", "\ ")
     return result_dir
 
 
