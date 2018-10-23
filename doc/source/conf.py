d6be35b7cfb300669fdec7e80a13ded6ad73723d
Håkon Austlid Taskén <hakon.tasken@gmail.com>
2018-10-24 01:27:48 +0200

    Add custum css (pygments.css) to conf.py.
    No need to copy it into the html any longer.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -90,6 +90,9 @@ pygments_style = 'sphinx'
 
 # -- Options for HTML output -------------------------------------------------
 
+def setup(app):
+    app.add_stylesheet('css/pygments.css')
+
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
