5688a73517d29d632a03a54136dadd7e4b494811
Håkon Austlid Taskén <hakon.tasken@gmail.com>
2018-11-27 18:09:30 +0100

    Remove non-ascii character from setup.py.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='sim_db',
     version='0.1.0',
     scripts=['sim_db', 'sdb', 'sim_db_cd_results.sh'],
-    author=u'Håkon Austlid Taskén',
+    author='Hakon Austlid Tasken',
     author_email='hakon.tasken@gmail.com',
     description='A database for simulation parameters.',
     long_description='A command line tool and set of functions for conveniently running a large number of simulations with different parameter values, while keeping track of these all simulation parameters and results along with metadata in a database for you.',
