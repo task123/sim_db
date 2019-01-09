48d63698ed3886a6ca21f32aca22f33f2c1b142e
Håkon Austlid Taskén <hakon.tasken@gmail.com>
2019-01-09 17:55:00 +0100

    Change from scripts to entry_points in setup.py.
    This should make it work on windows.
    - Add sim_db/__main__.py so '$python -m sim_db' can be used to invoke
    sim_db.

--- setup.py
+++ setup.py
@@ -2,21 +2,31 @@ import setuptools
 from sim_db import __version__
 
 setuptools.setup(
-    name='sim_db',
-    version=__version__,
-    scripts=['sim_db/sim_db', 'sim_db/sdb', 'sim_db/sim_db_cd.sh'],
-    author='Hakon Austlid Tasken',
-    author_email='hakon.tasken@gmail.com',
-    description='A database for simulation parameters.',
-    long_description='A command line tool and set of functions for conveniently running a large number of simulations with different parameter values, while keeping track of these all simulation parameters and results along with metadata in a database for you.',
-    url='http://sim-db.readthedocs.io',
-    packages=setuptools.find_packages(),
-    classifiers=[
-     "Programming Language :: Python",
-     "Programming Language :: Python :: 2",
-     "Programming Language :: Python :: 3",
-     "Programming Language :: Python",
-     "License :: OSI Approved :: MIT License",
-    ],
-    include_package_data=True,
+        name='sim_db',
+        version=__version__,
+        scripts=['sim_db/sim_db', 'sim_db/sdb', 'sim_db/sim_db_cd.sh'],
+        author='Hakon Austlid Tasken',
+        author_email='hakon.tasken@gmail.com',
+        description='A database for simulation parameters.',
+        long_description=
+        ('A command line tool and set of functions for '
+         'conveniently running a large number of simulations with different '
+         'parameter values, while keeping track of these all simulation '
+         'parameters and results along with metadata in a database for you.'),
+        url='http://sim-db.readthedocs.io',
+        packages=setuptools.find_packages(),
+        classifiers=[
+                'Programming Language :: Python',
+                'Programming Language :: Python :: 2',
+                'Programming Language :: Python :: 3',
+                'License :: OSI Approved :: MIT License',
+        ],
+        entry_points={
+                'console_scripts': [
+                        'sim_db = sim_db.__main__:sim_db',
+                        'sdb = sim_db.__main__:sdb'
+                ],
+        },
+        license='MIT License',
+        include_package_data=True,
 )
---
+++ sim_db/__main__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""sim_db command line tool entry points"""
+# Copyright (C) 2019 Håkon Austlid Taskén <hakon.tasken@gmail.com>
+# Licenced under the MIT License.
+
+import os.path
+import sys
+sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
+import sim_db.src_command_line_tool.command_line_tool as command_line_tool
+
+def sim_db():
+    """sim_db command line tool"""
+    command_line_tool.command_line_tool("sim_db", sys.argv[1:])
+
+def sdb():
+    """sdb command line tool"""
+    command_line_tool.command_line_tool("sdb", sys.argv[1:])
+
+if __name__ == '__main__':
+    sim_db()
