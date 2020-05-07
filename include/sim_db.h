8de886702728beaab6c7678927399a56b2104062
Håkon Austlid Taskén <hakon.tasken@gmail.com>
2020-05-07 11:57:25 +0200

    Update version to 0.2.9
    - All tests passes and all examples run.

--- include/sim_db.h
+++ include/sim_db.h
@@ -11,8 +11,8 @@
 #include <stdbool.h>
 #include <stdlib.h>  // To get definition of size_t.
 
-#define SIM_DB_VERSION "0.2.8"
-#define SIM_DB_VERSION_NUMBER 208
+#define SIM_DB_VERSION "0.2.9"
+#define SIM_DB_VERSION_NUMBER 209
 #define SIM_DB_MAJOR_VERSION_NUMBER SIM_DB_VERSION_NUMBER / 10000
 #define SIM_DB_MINOR_VERSION_NUMBER (SIM_DB_VERSION_NUMBER / 100) % 100
 #define SIM_DB_PATCH_VERSION_NUMBER SIM_DB_VERSION_NUMBER % 100
--- sim_db/__init__.py
+++ sim_db/__init__.py
@@ -2,6 +2,6 @@
 # Copyright (C) 2018-2020 Håkon Austlid Taskén <hakon.tasken@gmail.com>
 # Licenced under the MIT License.
 
-__version__ = '0.2.8'
+__version__ = '0.2.9'
 from sim_db.sim_db_lib import SimDB, add_empty_sim, ColumnError
 from sqlite3 import OperationalError
